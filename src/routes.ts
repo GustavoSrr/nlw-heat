1b5c3292ee27ce3e48ff2974fc8021bea3e68499
Gustavo <gustavosrodrigues2000@gmail.com>
2021-10-19 18:57:59 -0300

    ✨ Criando rota /profile

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateMessageController } from "./controllers/CreateMessageController";
 import { Get3LastMessagesController } from "./controllers/GetLast3MessagesController";
+import { ProfileUserController } from "./controllers/ProfileUserController";
 import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
 
 export const router = Router()
@@ -11,3 +12,5 @@ router.post("/authenticate", new AuthenticateUserController().handle)
 router.post("/messages", ensureAuthenticated, new CreateMessageController().handle)
 
 router.get("/messages/last3", new Get3LastMessagesController().handle)
+
+router.get("/profile", ensureAuthenticated, new ProfileUserController().handle)
