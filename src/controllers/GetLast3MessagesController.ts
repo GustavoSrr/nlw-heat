fc30910ce917ff41249a80de9a8b17a7f57eaf4a
Gustavo <gustavosrodrigues2000@gmail.com>
2021-10-19 18:38:14 -0300

    ✨ Criando controller para o GetLast3Messages

---
+++ src/controllers/GetLast3MessagesController.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+import { GetLast3MessageService } from "../services/GetLast3MessagesService";
+
+export class Get3LastMessagesController {
+  async handle (request: Request, response: Response) {
+    const service = new GetLast3MessageService()
+    const result = await service.execute()
+
+    return response.json(result)
+  }
+}
