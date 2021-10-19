054b5fad1a82cd128e70af7f2d2bca1f0649787d
Gustavo <gustavosrodrigues2000@gmail.com>
2021-10-19 15:47:06 -0300

    ✨ Usando cors e socket.io

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,37 @@
 import 'dotenv/config'
 import express from 'express'
+import http from 'http'
+import cors from 'cors'
+import { Server } from 'socket.io'
+
 import { router } from './routes'
 
 const app = express()
+
+app.use(cors())
+
+export const serverHttp = http.createServer(app)
+
+export const io = new Server(serverHttp, {
+  cors: {
+    origin: "*"
+  }
+})
+
+io.on('connection', socket => {
+  console.log(`Usuário conectado no socket ${socket.id}`)
+})
+
 app.use(express.json())
 
 app.use(router)
 
 app.get('/github', (request, response) => {
-  return response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`)
+  response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`)
 })
 
 app.get('/signin/callback', (request, response) => {
   const { code } = request.query
-  return response.json(code)
-})
 
-app.listen(4000, () => {
-  console.log('🚀 Servidor rodando na porta 4000.')
+  return response.json(code)
 })
