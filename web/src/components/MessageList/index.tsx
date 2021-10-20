503f16a25b529d43eaf986f2a4854849b9ee2a46
Gustavo <gustavosrodrigues2000@gmail.com>
2021-10-20 14:46:29 -0300

    ✨ Criando MessageList

--- web/src/components/MessageList/index.tsx
+++ web/src/components/MessageList/index.tsx
@@ -1,5 +1,40 @@
+import Logo from '../../assets/logo.svg'
+
+import styles from './styles.module.scss'
+
 export function MessageList() {
   return (
-    <h1>MessageList</h1>
+    <div className={styles.MessageListWrapper}>
+      <img src={Logo} alt="DoWhile" draggable={false} />
+      <ul className={styles.MessageList}>
+        <li className={styles.Message}>
+        <p className={styles.MessageContent}>Não vejo a hora de começar! 🚀</p>
+          <div className={styles.MessageAuthor}>
+            <div className={styles.AuthorAvatar}>
+              <img src="https://github.com/gustavosrr.png" alt="Gustavo Rodrigues" />
+            </div>
+            <span>Gustavo Rodrigues</span>
+          </div>
+        </li>
+        <li className={styles.Message}>
+          <p className={styles.MessageContent}>Não vejo a hora de começar! 🚀</p>
+          <div className={styles.MessageAuthor}>
+            <div className={styles.AuthorAvatar}>
+              <img src="https://github.com/gustavosrr.png" alt="Gustavo Rodrigues" />
+            </div>
+            <span>Gustavo Rodrigues</span>
+          </div>
+        </li>
+        <li className={styles.Message}>
+        <p className={styles.MessageContent}>Não vejo a hora de começar! 🚀</p>
+          <div className={styles.MessageAuthor}>
+            <div className={styles.AuthorAvatar}>
+              <img src="https://github.com/gustavosrr.png" alt="Gustavo Rodrigues" />
+            </div>
+            <span>Gustavo Rodrigues</span>
+          </div>
+        </li>
+      </ul>
+    </div>
   )
 }
