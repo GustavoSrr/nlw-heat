4660a0172f292fba97a4177739c0a302ffa238a9
Gustavo <gustavosrodrigues2000@gmail.com>
2021-10-20 16:18:06 -0300

    ✨ Usando context para pegar url de login

--- web/src/components/LoginBox/index.tsx
+++ web/src/components/LoginBox/index.tsx
@@ -1,12 +1,16 @@
+import { useContext } from 'react'
 import { FaGithub } from 'react-icons/fa'
+import { AuthContext } from '../../contexts/AuthContext'
 
 import styles from './styles.module.scss'
 
 export function LoginBox() {
+  const { signInUrl } = useContext(AuthContext)
+  
   return (
     <div className={styles.LoginBoxWrapper}>
       <strong>Entre e compartilhe sua mensagem.</strong>
-      <a href="" className={styles.SignInWithGithub}>
+      <a href={signInUrl} className={styles.SignInWithGithub}>
         <FaGithub size="24"/> Entrar com GitHub
       </a>
     </div>
