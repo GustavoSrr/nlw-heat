13979fa62b011f6d8664672864f19009892b4d0e
Gustavo <gustavosrodrigues2000@gmail.com>
2021-10-20 16:16:36 -0300

    ✨ Criando context para autenticação

---
+++ web/src/contexts/AuthContext.tsx
@@ -0,0 +1,80 @@
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { api } from "../services/api";
+
+type User = {
+  name: string;
+  login: string;
+  avatar_url: string;
+  id: string;
+}
+
+type AuthContextData = {
+  user: User | null;
+  signInUrl: string;
+  signOut: () => void;
+}
+
+export const AuthContext = createContext({} as AuthContextData)
+
+type AuthProvider = {
+  children: ReactNode;
+}
+
+type AuthResponse = {
+  token: string;
+  user: {
+    name: string;
+    login: string;
+    avatar_url: string;
+    id: string;
+  }
+}
+
+export function AuthProvider(props: AuthProvider) {
+  const [user, setUser] = useState<User | null>(null)
+
+  const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=5727f744773b10b0a84b`
+
+  async function signIn(gitHubCode: string) {
+    const response = await api.post<AuthResponse>('authenticate', {
+      code: gitHubCode
+    })
+
+    const { token, user } = response.data
+    localStorage.setItem('@dowhile:token', token)
+    setUser(user)
+  }
+
+  async function signOut() {
+    setUser(null)
+    localStorage.removeItem('@dowhile:token')
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem('@dowhile:token')
+
+    if(token) {
+      api.defaults.headers.common.authorization = `Bearer ${token}`
+      api.get<User>('profile').then(response => {
+        setUser(response.data)
+      })
+    }
+  }, [])
+
+  useEffect(() => {
+    const url = window.location.href
+    const hasGitHubCode = url.includes('?code=')
+    if(hasGitHubCode) {
+      const [urlWithoutCode, gitHubCode] = url.split('?code=')
+
+      window.history.pushState({}, '', urlWithoutCode)
+      signIn(gitHubCode)
+    }
+  }, [])
+
+  return (
+    <AuthContext.Provider value={{ user, signInUrl, signOut }}>
+      {props.children}
+    </AuthContext.Provider>
+  )
+}
