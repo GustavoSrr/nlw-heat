33b356b1c30c0f71f57ec72b3d15839310110ad1
Gustavo <gustavosrodrigues2000@gmail.com>
2021-10-20 15:03:33 -0300

    ✨ Fazendo chamada para API

--- web/src/components/MessageList/index.tsx
+++ web/src/components/MessageList/index.tsx
@@ -1,39 +1,47 @@
+import { useEffect, useState } from 'react'
+import { api } from '../../services/api'
+
 import Logo from '../../assets/logo.svg'
 
 import styles from './styles.module.scss'
 
+type Message = {
+  id: string;
+  text: string;
+  user: {
+    name: string;
+    avatar_url: string;
+  }
+}
+
 export function MessageList() {
+  const [messages, setMessages] = useState<Message[]>([])
+
+  useEffect(() => {
+    api.get<Message[]>('messages/last3').then(response => {
+      setMessages(response.data)
+    })
+  }, [])
+
   return (
     <div className={styles.MessageListWrapper}>
       <img src={Logo} alt="DoWhile" draggable={false} />
       <ul className={styles.MessageList}>
-        <li className={styles.Message}>
-        <p className={styles.MessageContent}>Não vejo a hora de começar! 🚀</p>
-          <div className={styles.MessageAuthor}>
-            <div className={styles.AuthorAvatar}>
-              <img src="https://github.com/gustavosrr.png" alt="Gustavo Rodrigues" />
-            </div>
-            <span>Gustavo Rodrigues</span>
-          </div>
-        </li>
-        <li className={styles.Message}>
-          <p className={styles.MessageContent}>Não vejo a hora de começar! 🚀</p>
-          <div className={styles.MessageAuthor}>
-            <div className={styles.AuthorAvatar}>
-              <img src="https://github.com/gustavosrr.png" alt="Gustavo Rodrigues" />
-            </div>
-            <span>Gustavo Rodrigues</span>
-          </div>
-        </li>
-        <li className={styles.Message}>
-        <p className={styles.MessageContent}>Não vejo a hora de começar! 🚀</p>
-          <div className={styles.MessageAuthor}>
-            <div className={styles.AuthorAvatar}>
-              <img src="https://github.com/gustavosrr.png" alt="Gustavo Rodrigues" />
-            </div>
-            <span>Gustavo Rodrigues</span>
-          </div>
-        </li>
+        {
+          messages.map(message => {
+            return (
+              <li className={styles.Message} key={message.id}>
+                <p className={styles.MessageContent}>{message.text}</p>
+                <div className={styles.MessageAuthor}>
+                  <div className={styles.AuthorAvatar}>
+                    <img src={message.user.avatar_url} alt={message.user.name} />
+                  </div>
+                  <span>{message.user.name}</span>
+                </div>
+              </li>
+            )
+          })
+        }
       </ul>
     </div>
   )
